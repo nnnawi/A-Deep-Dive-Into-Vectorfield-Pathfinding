fc1b10c14fbe3ffbc474f21f3acae1949b50093d
Iwan Bnlf <iwan@ibnoulouafi.com>
2026-05-26 15:44:34 -0700

    feat: added github page hosting

--- vectorfield-blogpost/astro.config.mjs
+++ vectorfield-blogpost/astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import remarkMath from 'remark-math';
+import { defineConfig } from 'astro/config';
 import rehypeMathjax from 'rehype-mathjax';
+import remarkMath from 'remark-math';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://nnnawi.github.io/VectorField-Pathfinding-Blog/',
+  base: '/VectorField-Pathfinding-Blog/',
+  output: 'static',
   integrations: [
     mdx({
       remarkPlugins: [remarkMath],
